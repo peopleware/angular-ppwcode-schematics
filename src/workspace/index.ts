ecc24d682fd7157f2227fcd80fd60062feaa0d95
Adriaan Peeters <adriaan_peeters@peopleware.be>
2020-04-07 10:46:59 +0200

    Support overriding workspace files

--- src/workspace/index.ts
+++ src/workspace/index.ts
@@ -1,13 +1,26 @@
 import {
+  apply,
+  applyTemplates,
   chain,
   externalSchematic,
+  MergeStrategy,
+  mergeWith,
   Rule,
+  url,
 } from '@angular-devkit/schematics';
+import {strings} from "@angular-devkit/core";
 
 export default function(options: object): Rule {
   return () => {
     return chain([
       externalSchematic('@schematics/angular', 'workspace', options),
+      mergeWith(apply(url('./files'), [
+        applyTemplates({
+          utils: strings,
+          ...options,
+          'dot': '.'
+        }),
+      ]), MergeStrategy.AllowCreationConflict),
     ]);
   };
 }
