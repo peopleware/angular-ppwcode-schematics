bcb2783e532069be6340481266f85cdcc59659ea
Adriaan Peeters <adriaan_peeters@peopleware.be>
2020-04-08 12:17:10 +0200

    Always enable routing

--- src/application/index.ts
+++ src/application/index.ts
@@ -2,15 +2,25 @@ import {
   chain,
   externalSchematic,
   Rule,
+  SchematicsException,
 } from '@angular-devkit/schematics';
 
 interface ApplicationOptions {
+  routing: boolean;
 }
 
 export default function(options: ApplicationOptions): Rule {
   return () => {
+    setRouting(options);
     return chain([
       externalSchematic('@schematics/angular', 'application', options),
     ]);
   };
+}
+
+function setRouting(options: ApplicationOptions) {
+  if (options.routing !== undefined) {
+    throw new SchematicsException(`Invalid option: routing`);
+  }
+  options.routing = true;
 }
