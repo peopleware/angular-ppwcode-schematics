891cf1c8b088251baad1cb5be1bf1a8d55137e48
Adriaan Peeters <adriaan_peeters@peopleware.be>
2020-04-08 12:14:54 +0200

    Always set style to scss
    # Conflicts:
    #	src/application/index.ts

--- src/application/index.ts
+++ src/application/index.ts
@@ -7,11 +7,13 @@ import {
 
 interface ApplicationOptions {
   routing: boolean;
+  style: string;
 }
 
 export default function(options: ApplicationOptions): Rule {
   return () => {
     setRouting(options);
+    setStyle(options);
     return chain([
       externalSchematic('@schematics/angular', 'application', options),
     ]);
@@ -23,4 +25,11 @@ function setRouting(options: ApplicationOptions) {
     throw new SchematicsException(`Invalid option: routing`);
   }
   options.routing = true;
+}
+
+function setStyle(options: ApplicationOptions) {
+  if (options.style !== undefined) {
+    throw new SchematicsException(`Invalid option: style`);
+  }
+  options.style = 'scss';
 }
