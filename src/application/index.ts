01a209a924be4495b17c039f5f7a91a323fab957
Adriaan Peeters <adriaan_peeters@peopleware.be>
2020-04-09 16:23:24 +0200

    Remove duplicate style setting

--- src/application/index.ts
+++ src/application/index.ts
@@ -4,10 +4,13 @@ import {
   Rule,
   SchematicsException,
 } from '@angular-devkit/schematics';
+import {updateWorkspace} from "@schematics/angular/utility/workspace";
+import {JsonObject} from "@angular-devkit/core";
 
 interface ApplicationOptions {
   routing: boolean;
   style: string;
+  name: string;
 }
 
 export default function(options: ApplicationOptions): Rule {
@@ -16,6 +19,7 @@ export default function(options: ApplicationOptions): Rule {
     setStyle(options);
     return chain([
       externalSchematic('@schematics/angular', 'application', options),
+      removeDuplicateStyle(options),
     ]);
   };
 }
@@ -32,4 +36,23 @@ function setStyle(options: ApplicationOptions) {
     throw new SchematicsException(`Invalid option: style`);
   }
   options.style = 'scss';
+}
+
+function removeDuplicateStyle(options: ApplicationOptions) {
+  return updateWorkspace(workspace => {
+    const project = workspace.projects.get(options.name);
+    if (!project) {
+      throw new SchematicsException(`Invalid project name (${options.name})`);
+    }
+    const extensions = project.extensions;
+    const schematics = JSON.parse(JSON.stringify(extensions.schematics));
+    const component = schematics["@schematics/angular:component"] as JsonObject;
+    if (component["style"] === "scss") {
+      delete component["style"];
+    }
+    if (Object.keys(component).length === 0) {
+      delete schematics["@schematics/angular:component"];
+    }
+    extensions.schematics = schematics;
+  });
 }
