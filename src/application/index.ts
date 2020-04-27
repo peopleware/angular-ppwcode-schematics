89a9a1f4326c52b34d1f4ca6ab990feff0075279
Adriaan Peeters <adriaan_peeters@peopleware.be>
2020-04-27 16:37:00 +0200

    Add skipInstall to ApplicationOptions

--- src/application/index.ts
+++ src/application/index.ts
@@ -14,6 +14,7 @@ export interface ApplicationOptions {
   routing?: boolean;
   style?: string;
   name: string;
+  skipInstall?: boolean;
 }
 
 export default function(options: ApplicationOptions): Rule {
