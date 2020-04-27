b7606510c40b8b8dfa0caeb4322fa2f365e38f64
Adriaan Peeters <adriaan_peeters@peopleware.be>
2020-04-27 16:40:05 +0200

    Call workspace and application ourselves in ng-new

--- src/ng-new/index.ts
+++ src/ng-new/index.ts
@@ -1,13 +1,38 @@
 import {
   chain,
-  externalSchematic,
+  move,
   Rule,
+  schematic,
+  SchematicsException,
 } from '@angular-devkit/schematics';
+import {ApplicationOptions} from "../application";
+import {WorkspaceOptions} from "../workspace";
 
 export default function(options: any): Rule {
+  if (!options.name) {
+    throw new SchematicsException(`Invalid options, "name" is required.`);
+  }
+
+  if (!options.directory) {
+    options.directory = options.name;
+  }
+
+  const workspaceOptions: WorkspaceOptions = {
+    name: options.name,
+    style: options.style,
+  };
+
+  const applicationOptions: ApplicationOptions = {
+    projectRoot: '',
+    name: options.name,
+    skipInstall: true,
+  };
+
   return () => {
     return chain([
-      externalSchematic('@schematics/angular', 'ng-new', options),
+      schematic('workspace', workspaceOptions),
+      schematic('application', applicationOptions),
+      move(options.directory),
     ]);
   };
 }
