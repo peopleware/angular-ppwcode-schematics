f911391ff76738b030da608ff22b708b6828b7ca
Adriaan Peeters <adriaan_peeters@peopleware.be>
2020-09-15 11:54:23 +0200

    Release 1.1.2

--- src/workspace/index.ts
+++ src/workspace/index.ts
@@ -62,7 +62,7 @@ function addDependenciesToPackageJson() {
       {
         type: NodeDependencyType.Dev,
         name: '@ppwcode/angular-schematics',
-        version: '1.1.1',
+        version: '1.1.2',
       },
       {
         type: NodeDependencyType.Dev,
