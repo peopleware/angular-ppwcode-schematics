bf41d9082c24d0ddc99abda96ce34cdff6c344ba
tim_frijters <tim_frijters@peopleware.be>
2021-06-14 14:24:35 +0200

    Release 2.1.1

--- src/workspace/index.ts
+++ src/workspace/index.ts
@@ -62,7 +62,7 @@ function addDependenciesToPackageJson() {
       {
         type: NodeDependencyType.Dev,
         name: '@ppwcode/angular-schematics',
-        version: '2.1.0',
+        version: '2.1.1',
       },
       {
         type: NodeDependencyType.Dev,
