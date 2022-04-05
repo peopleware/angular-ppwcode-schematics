c56b2775e44e3472f00832b578730326b01b3911
Maarten Somers <maarten_somers@peopleware.be>
2022-04-05 16:09:22 +0200

    Release 4.0.0

--- src/workspace/index.ts
+++ src/workspace/index.ts
@@ -62,7 +62,7 @@ function addDependenciesToPackageJson() {
       {
         type: NodeDependencyType.Dev,
         name: '@ppwcode/angular-schematics',
-        version: '3.2.0',
+        version: '4.0.0',
       },
       {
         type: NodeDependencyType.Dev,
